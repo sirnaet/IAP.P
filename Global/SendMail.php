0d81ecfcd371ef230faf096b9a45c04d99900808
azharhacks <azharabeid5@gmail.com>
2025-09-11 13:14:46 +0300

    added a cooment in SendMail file

--- Global/SendMail.php
+++ Global/SendMail.php
@@ -1,10 +1,11 @@
 <?php
+//Used this file instead of mail.php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . '/../Plugins/PHPMailer/vendor/autoload.php';
-
 
 
 class SendMail {
